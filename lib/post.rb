2ad5bed797e89f801706ff89e9f358a4bff2f26e
M Banks <montensia1@outlook.com>
2019-05-29 16:22:10 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,13 @@
 class Post 
- # attr_accessor :name, posts
+  attr_accessor :post, :title
+  @@all = []
+  def initialize(title)
+    @title = title
+    @all = self
+  end
+  
+  def self.all
+  end
 
   
 end
